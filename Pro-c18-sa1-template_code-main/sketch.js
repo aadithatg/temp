af8f3faa1f0bfdb034b81c92452c101778122a8b
Aadith A <88674824+aadithatg@users.noreply.github.com>
2021-10-09 09:49:04 +0100

    Add files via upload

---
+++ Pro-c18-sa1-template_code-main/sketch.js
@@ -0,0 +1,12 @@
+let Student
+function setup() {
+  createCanvas(400,400);
+Student = new student("aadith",13,"male")
+Student.show()
+
+}
+
+function draw() 
+{
+  background(30);
+}
---
+++ Pro-c18-sa1-template_code-main/student.js
@@ -0,0 +1,17 @@
+class student{
+constructor(name,age,gender){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+}
+show(){
+    console.log(this.name);
+    console.log(this.age);
+    console.log(this.gender);
+
+}
+}
+
+
+
+
